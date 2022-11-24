dd53401d235f429606283ec43b158ba1662c8e70
Rido Atmanto <ridoatmanto@gmail.com>
2022-11-24 23:01:58 +0700

    Refactor book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,10 @@
 class Book < ApplicationRecord
 	has_many :book_author
-  # scope :filter_by_author_id, -> (author_id) { where author_id: author_id }
-  # scope :filter_by_year, -> (year) { where year: year }
+	
+  scope :filter_by_author_id, -> (author_id) {
+  	joins(:book_author).where(book_authors: {author_id: author_id})
+  }
+  scope :filter_by_year, -> (year) { where year: year }
   scope :filter_by_minimum_book_page, -> (book_page) { 
   	where("page >= ", book_page)
   }
@@ -11,20 +14,4 @@ class Book < ApplicationRecord
   scope :filter_by_range_book_page, -> (book_page_min, book_page_max) { 
   	where("page >= ? AND page <= ?", book_page_min, book_page_max)
   }
-
-  def self.filter_by_year(year = nil)
-	  if year.present?
-	    where(year: year)
-	  else
-	  	all
-	  end
-	end
-
-  def self.filter_by_author_id(author_id = nil)
-	  if author_id.present?
-	  	joins(:book_author).where(book_authors: {author_id: author_id})
-	  else
-	  	all
-	  end
-	end
 end
