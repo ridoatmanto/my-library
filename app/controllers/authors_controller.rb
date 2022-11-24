657b6c146e1e9c7317c0b968633969260bc5c28d
Rido Atmanto <ridoatmanto@gmail.com>
2022-11-24 18:37:04 +0700

    Add show author detail

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,32 @@
 class AuthorsController < ApplicationController
+	def index
+    @author = Author.all
+  end
+
+  def show
+    @author = Author.find(params[:id])
+    # @gender_list = {m: "Male", f: "Female"}
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to @author
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(
+      :name, :gender, :bio, :born, :died, :summary
+    )
+  end
 end
