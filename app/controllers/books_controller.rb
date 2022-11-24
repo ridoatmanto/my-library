d36a8b573d895aa46b61208f12f73658870748f3
Rido Atmanto <ridoatmanto@gmail.com>
2022-11-24 16:53:01 +0700

    Show book detail

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,32 @@
 class BooksController < ApplicationController
-  def index
+def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to @book
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(
+    	:title, :year, :plot, :copyright,
+    	:description, :language, :release_date, :cover,
+    	:summary, :classes)
   end
 end
